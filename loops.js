cc9d706ccb09eb7c091d3b39e6f8f5d85ec8f4b5
Sam Belliel <sbelliel@gmail.com>
2018-07-26 23:33:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,11 @@
 function ForLoop(array) {
 }
 
+for ([var i = 0]; [i < 25]; [i++]) {
+  array[i] =  "I am ${i} strange loop${i === 0 ? '' : 's'}."
+
+return array
+
 function whileLoop(n) {
   while (n > 0) {
     console.log(--n)
